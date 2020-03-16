d6b73878f479f253a363f146a8c2b6488ec797ac
guswn2521 <61112694+guswn2521@users.noreply.github.com>
2020-03-16 18:46:54 +0900

    backjoon quick A+B

--- javabasic/src/bj/Main.java
+++ javabasic/src/bj/Main.java
@@ -1,26 +1,77 @@
 package bj;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
+		BufferedReader bfr = null;
+		BufferedWriter bfw = null;
+		FileWriter fw = null;
+		FileInputStream fi = null;
+		InputStreamReader isr = null;
 		Scanner sc = new Scanner(System.in);
-		int x = sc.nextInt();
-		int y = sc.nextInt();
-		int Quadrant = 0;
-		if(x*y > 0) {
-			if(x > 0) {
-				Quadrant = 1;
-			}else {
-				Quadrant = 3;
+		
+		int T = sc.nextInt();
+		try {
+			fw = new FileWriter("C:/Users/205/git/add.txt",true);
+			bfw = new BufferedWriter(fw);
+			
+			for(int i=0;i<T;i++) {
+				int x = (int)(Math.random()*1000)+1;
+				int y = (int)(Math.random()*1000)+1;
+				bfw.write(x);
+				bfw.write(y);
+				int add = x+y;
+				bfw.write(add+"\r\n");
+				
 			}
-		}else {
-			if(x>0) {
-				Quadrant = 4;
-			}else Quadrant = 2;
+			bfw.flush();
+		} catch (Exception e) {
+			e.printStackTrace();
 		}
-		System.out.println(Quadrant);
+		
+//		try {
+//			fi = new FileInputStream("C:/Users/205/git/add.txt");
+//			isr = new InputStreamReader(fi);
+//			bfr = new BufferedReader(isr);
+//			
+//			String str = null;
+//			while((str = bfr.readLine())!=null) {
+//				System.out.println(str);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+		finally {
+			try {
+				//fi.close();
+				//isr.close();
+				sc.close();
+				//bfr.close();
+				bfw.close();
+				fw.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+		
+		
+		
+//		for(int i=0;i<T;i++) {
+//			int x, y;
+//			x = sc.nextInt();
+//			y = sc.nextInt();
+//			int add = x+ y;
+//			System.out.println(add);
+//		}
 
 	}
 
