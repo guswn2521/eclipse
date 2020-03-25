f56fce6bca44c1c219e34267bb113156d59b1b0c
guswn2521 <61112694+guswn2521@users.noreply.github.com>
2020-03-25 18:47:20 +0900

    .

--- javabasic/src/bj/Main.java
+++ javabasic/src/bj/Main.java
@@ -1,78 +1,27 @@
 package bj;
 
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
+
 public class Main {
-
+	
 	public static void main(String[] args) {
-		BufferedReader bfr = null;
-		BufferedWriter bfw = null;
-		FileWriter fw = null;
-		FileInputStream fi = null;
-		InputStreamReader isr = null;
 		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-		try {
-			fw = new FileWriter("C:/Users/205/git/add.txt",true);
-			bfw = new BufferedWriter(fw);
-			
-			for(int i=0;i<T;i++) {
-				int x = (int)(Math.random()*1000)+1;
-				int y = (int)(Math.random()*1000)+1;
-				bfw.write(x);
-				bfw.write(y);
-				int add = x+y;
-				bfw.write(add+"\r\n");
-				
-			}
-			bfw.flush();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		
-//		try {
-//			fi = new FileInputStream("C:/Users/205/git/add.txt");
-//			isr = new InputStreamReader(fi);
-//			bfr = new BufferedReader(isr);
-//			
-//			String str = null;
-//			while((str = bfr.readLine())!=null) {
-//				System.out.println(str);
-//			}
-//		} catch (Exception e) {
-//			e.printStackTrace();
-//		}
-		finally {
-			try {
-				//fi.close();
-				//isr.close();
-				sc.close();
-				//bfr.close();
-				bfw.close();
-				fw.close();
-			} catch (Exception e) {
-				e.printStackTrace();
+		int n = sc.nextInt();
+		double[] scores = new double[n];
+		int m=0;
+		for(int i=0;i<n;i++) {
+			int sjt = sc.nextInt();
+			scores[i] = sjt;
+			if(sjt > m) {
+				m = sjt;
 			}
 		}
-		
-		
-		
-//		for(int i=0;i<T;i++) {
-//			int x, y;
-//			x = sc.nextInt();
-//			y = sc.nextInt();
-//			int add = x+ y;
-//			System.out.println(add);
-//		}
-
+		double sum = 0;
+		for(int i=0;i<n;i++) {
+			double d = (double)scores[i]*100 / m;
+			sum = sum + d;
+		}
+		System.out.println(sum / n);
 	}
-
 }
