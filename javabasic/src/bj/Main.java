9cb6c069b5dc5368ee62a21829e2683cc228206a
guswn2521 <61112694+guswn2521@users.noreply.github.com>
2020-03-27 18:47:35 +0900

    .

--- javabasic/src/bj/Main.java
+++ javabasic/src/bj/Main.java
@@ -1,17 +1,42 @@
 package bj;
 
-import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Main {
 	
 	public static void main(String[] args) {
+		ArrayList<Integer> arr = new ArrayList<Integer>();
+		
+		for(int i=1;i<10001;i++) {
+			arr.add(i);
+		}
+		
+		for(int i=0;i<10000;i++) {
+			for(int j=0;j<arr.size();j++) {
+				if(d(i) == arr.get(j)) {
+					arr.remove(j);
+				}
+			}
+		}
+		for(int i=0;i<arr.size();i++) {
+			System.out.println(arr.get(i));
+		}
+		
+		int n=0;
+		while(n<=10000) {
+			n++;
+			n = d(n);
+		}
+		
+		
+		
 		
 	}
 	static int d(int n) {
-		int sum = 0;
+		int sum = n;
 		while(n != 0) {
-			sum+= (n%10);
-			n = n /10;
+			sum = sum + n%10;
+			n = n/10;
 		}
 		return sum;
 	}
